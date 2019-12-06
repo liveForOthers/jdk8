756ec100afc4d0619bac08b6bad76d5e731b80b1
王作军 <wangzuojun@meituan.com>
2019-12-06 10:43:15 +0800

    CopyOnWriteArrayList

--- src/java/util/concurrent/CopyOnWriteArrayList.java
+++ src/java/util/concurrent/CopyOnWriteArrayList.java
@@ -88,15 +88,40 @@ import sun.misc.SharedSecrets;
  * @since 1.5
  * @author Doug Lea
  * @param <E> the type of elements held in this collection
+ *
+ * CopyOnWrite容器即写时复制的容器。
+ * 当我们往一个容器添加元素的时候，
+ *    不直接往当前容器添加，
+ *    而是先将当前容器进行Copy，复制出一个新的容器，然后新的容器里添加元素，添加完元素之后，再将原容器的引用指向新的容器。
+ *    这样做的好处是我们可以对CopyOnWrite容器进行  并发的读，而不需要加锁，因为当前容器不会添加任何元素。
+ *    所以CopyOnWrite容器也是一种读写分离的思想，读和写不同的容器。
+ *
+ * 应用场景：
+ * CopyOnWrite并发容器用于读多写少的并发场景。比如白名单，黑名单，商品类目的访问和更新场景，假如我们有一个搜索网站，用户在这个网站的搜索框中，输入关键字搜索内容，但是某些关键字不允许被搜索。这些不能被搜索的关键字会被放在一个黑名单当中，黑名单每天晚上更新一次。当用户搜索时，会检查当前关键字在不在黑名单当中，如果在，则提示不能搜索。
+ *
+ * 使用时应注意：
+*     1 预估容量  减少容器扩容开销
+ *    2 增加数据尽量 批量增加  减少新容器创建次数
+ *
+ * 内存占用问题和数据一致性问题。所以在开发的时候需要注意一下。
+ *
+ * 　　3 内存占用问题。因为CopyOnWrite的写时复制机制，所以在进行写操作的时候，内存里会同时驻扎两个对象的内存，旧的对象和新写入的对象（注意:在复制的时候只是复制容器里的引用，只是在写的时候会创建新对象添加到新容器里，而旧容器的对象还在使用，所以有两份对象内存）。如果这些对象占用的内存比较大，比如说200M左右，那么再写入100M数据进去，内存就会占用300M，那么这个时候很有可能造成频繁的Yong GC和Full GC。之前我们系统中使用了一个服务由于每晚使用CopyOnWrite机制更新大对象，造成了每晚15秒的Full GC，应用响应时间也随之变长。
+ *
+ * 　　针对内存占用问题，可以通过压缩容器中的元素的方法来减少大对象的内存消耗，比如，如果元素全是10进制的数字，可以考虑把它压缩成36进制或64进制。或者不使用CopyOnWrite容器，而使用其他的并发容器，如ConcurrentHashMap。
+ *
+ * 　　4 数据一致性问题。CopyOnWrite容器只能保证数据的最终一致性，不能保证数据的实时一致性。所以如果你希望写入的的数据，马上能读到，请不要使用CopyOnWrite容器。
+ *
  */
 public class CopyOnWriteArrayList<E>
     implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
     private static final long serialVersionUID = 8673264195747942595L;
 
     /** The lock protecting all mutators */
+    // 基于重入锁实现
     final transient ReentrantLock lock = new ReentrantLock();
 
     /** The array, accessed only via getArray/setArray. */
+    // 数据容器  仅可通过 getArray/setArray 拿到容器对象
     private transient volatile Object[] array;
 
     /**
@@ -116,6 +141,8 @@ public class CopyOnWriteArrayList<E>
 
     /**
      * Creates an empty list.
+     *
+     * 构造方法 初始化一个 空容器
      */
     public CopyOnWriteArrayList() {
         setArray(new Object[0]);
@@ -392,6 +419,8 @@ public class CopyOnWriteArrayList<E>
      * {@inheritDoc}
      *
      * @throws IndexOutOfBoundsException {@inheritDoc}
+     *
+     * 读容器时无需加锁   直接查询当前容器的 第index位的 值
      */
     public E get(int index) {
         return get(getArray(), index);
@@ -433,15 +462,22 @@ public class CopyOnWriteArrayList<E>
      */
     public boolean add(E e) {
         final ReentrantLock lock = this.lock;
+        // 写时加锁
         lock.lock();
         try {
+            // 拿到原容器
             Object[] elements = getArray();
+            // 原容器容量
             int len = elements.length;
+            // 创建一个 容量为 原容量+1 的 新容器
             Object[] newElements = Arrays.copyOf(elements, len + 1);
+            // 将新元素 插入到原容器最后位置
             newElements[len] = e;
+            // 用新容器 替换原容器
             setArray(newElements);
             return true;
         } finally {
+            // 释放锁
             lock.unlock();
         }
     }
